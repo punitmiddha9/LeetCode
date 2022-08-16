23ce5f8ddd239bb994283d6ddbccb4b04a0f6155
Punit Middha <70194627+punitmiddha9@users.noreply.github.com>
2022-08-17 01:40:30 +0530

    Time: 17 ms (44.62%), Space: 7.2 MB (94.49%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
+        unordered_map<char, int> mp;
+        for(int i=0; i<s.size(); i++){
+            mp[s[i]]++;
+        }
+        for(int j=0; j<t.size(); j++){
+            mp[t[j]]--;
+        }
+        for(auto x : mp){
+            if(x.second > 0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
